6c8b863c44dfb66b28cc4e2ca64e56ec53bb96a5
Ange <angela.sepulveda1@mail.udp.cl>
2026-03-27 12:19:10 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  let c= color (0,0,0);
+  fill(c);
+  ellipse (65, 350, 60, 60)
+ellipse (130, 350, 60, 60)
+ ellipse (195, 350, 60, 60)
+ellipse (260, 350, 60, 60)
+ellipse (325, 350, 60, 60)
+ellipse (65, 285, 60, 60)
+ellipse (130, 285, 60, 60)
+ellipse (195, 285, 60, 60)
+ellipse (195,285, 60, 60)
+}
